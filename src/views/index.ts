64eae610547749ec1cfc26c8cc69ddec28fb93e9
Andreas Schönlein <andreasschoenlein@icloud.com>
2026-01-27 08:45:19 +0100

    Wire views index and viewsOrder

---
+++ src/views/index.ts
@@ -0,0 +1,7 @@
+export { default as Master } from "./Master";
+export { default as Cases } from "./Cases";
+export { default as Evidence } from "./Evidence";
+export { default as Archive } from "./Archive";
+export { default as Settings } from "./Settings";
+
+export * from "./viewsOrder";
