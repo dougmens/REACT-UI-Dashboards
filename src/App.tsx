5dfde5974d2ea21c5d666643fdd6a0ae8574fe27
Andreas Schönlein <andreasschoenlein@icloud.com>
2026-01-27 09:41:54 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,11 @@ import Layout from "./components/Layout";
 export default function App() {
   return (
     <Routes>
-      {/* Layout-Route */}
       <Route element={<Layout />}>
-        {/* Default → erste View */}
         <Route path="/" element={<Navigate to={VIEWS_ORDER[0].path} replace />} />
-
-        {/* Views */}
         {VIEWS_ORDER.map((v) => (
           <Route key={v.key} path={v.path} element={<v.Component />} />
         ))}
-
-        {/* Fallback */}
         <Route path="*" element={<Navigate to={VIEWS_ORDER[0].path} replace />} />
       </Route>
     </Routes>
